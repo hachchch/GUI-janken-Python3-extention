521e7fd4feff8cfbd59a9c1a4960f298c98bc128
PGAT begginer <137990341+hachchch@users.noreply.github.com>
2024-01-18 11:17:10 +0900

    Update gui.py

--- LocalMultiplayerVersion/gui.py
+++ LocalMultiplayerVersion/gui.py
@@ -1,1 +1,28 @@
+from tkinter import *
+import LocalMultiplayerVersion.janken as jk
+root = Tk()
+root.title("janken")
 
+label = Label(text="choose player1 hand")
+label.pack()
+
+r_button = Button(
+    text="rock",
+    width=10,
+    command=lambda:jk.janken(0)
+)
+p_button = Button(
+    text="paper",
+    width=10,
+    command=lambda:jk.janken(1)
+)
+s_button = Button(
+    text="scissors",
+    width=10,
+    command=lambda:jk.janken(2)
+)
+r_button.pack()
+p_button.pack()
+s_button.pack()
+
+root.mainloop()
