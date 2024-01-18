52181f97977e18ac75bedd7ca3086f8dc51b7f7b
PGAT begginer <137990341+hachchch@users.noreply.github.com>
2024-01-18 11:23:08 +0900

    Create janken.py

---
+++ LocalMultiplayerVersion/janken.py
@@ -0,0 +1,19 @@
+import tkinter.messagebox as mb
+
+def janken(you):
+    hands = ['rock','paper','scissors']
+    you_hand = hands[you]
+def jankenII(youII):
+    hands = ['rock','paper','scissors']
+    youII_hand = hands[com]
+
+    j = (you - youII +3) % 3
+    if j == 0:
+        txt = 'draw: '
+    elif j == 1:
+        txt = 'Player1 win!: '
+    elif j == 2:
+        txt = 'Player2 win!: '
+
+    txt += ' Player1:' + you_hand + ' Player2:' + youII_hand
+    mb.showinfo('result',txt)
