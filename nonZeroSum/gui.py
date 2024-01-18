589c50071a20e3c4070d9fcfd891921710c2fb59
PGAT begginer <137990341+hachchch@users.noreply.github.com>
2024-01-18 10:53:12 +0900

    Update gui.py

--- nonZeroSum/gui.py
+++ nonZeroSum/gui.py
@@ -10,22 +10,22 @@ label.pack()
 r_button = Button(
     text="rock",
     width=10,
-    command=lambda:jk.janken(0)
+    command=lambda:jk.janken(1)
 )
 p_button = Button(
     text="paper",
     width=10,
-    command=lambda:jk.janken(1)
+    command=lambda:jk.janken(2)
 )
 s_button = Button(
     text="scissors",
     width=10,
-    command=lambda:jk.janken(2)
+    command=lambda:jk.janken(3)
 )
 e_button = Button(
     text="shoe",
     width=10,
-    command=lambda:jk.janken(3)
+    command=lambda:jk.janken(4)
 )
 r_button.pack()
 p_button.pack()
